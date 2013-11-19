4f2e00588c4f0188bde76c903d789951e387fcce
T. Alexander <t.alex@outlook.com>
2013-11-18 19:53:05 -0500

    Update Flatspace.java
    Alpha 1.3: Large blocks harder to destroy, reset added for the timer, actual seconds clock added, splash screen, and title of window and save button fixed.

--- src/Flatspace.java
+++ src/Flatspace.java
@@ -1,4 +1,4 @@
-//FlatSpace Alpha 1.2
+//FlatSpace Alpha 1.3: large blocks harder to destroy, reset for long timer, clock, splash, title/screenshot window.
 import java.util.*;
 public class Flatspace {
 	public static ArrayList<Double> XObject = new ArrayList<Double>();
@@ -10,6 +10,9 @@ public class Flatspace {
 		Random rng = new Random();
 		StdDraw.setXscale(-1.0, 1.0);
 		StdDraw.setYscale(-1.0, 1.0);
+		StdDraw.picture(0, 0, "Splash1.jpg", 2.2, 2.2);
+		StdDraw.show(0);
+		while (!StdDraw.mousePressed()) {}
 		while (true) {
 			XObject.clear();
 			YObject.clear();
@@ -18,7 +21,7 @@ public class Flatspace {
 			TypeObject.clear();
 			StdDraw.setPenColor(StdDraw.BLACK);
 			StdDraw.filledSquare(0, 0, 2);
-			double XShip = 0, YShip = 0, ShipAngle, XMouse, YMouse;
+			double XShip = 0, YShip = 0, ShipAngle, XMouse, YMouse, ProbabilityLargeDelete = .9, StartTime = System.currentTimeMillis();
 			boolean DebugMenu = false, LoopRunning = true, GameRunning = true, Invincible = false, BulletsOn = true;
 			int BulletScore = 0, KeyTimeout = 0, GameScore = 0, DetectCount = 0, LongTimer = 0;
 			while (GameRunning) {
@@ -45,7 +48,7 @@ public class Flatspace {
 						VXObject.add(.04 * Math.cos(Math.toRadians(ShipAngle + 90)));
 						VYObject.add(.04 * Math.sin(Math.toRadians(ShipAngle + 90)));
 						TypeObject.add(2.0); } }
-			//Creates Boxes Every 30 Ticks, Resets Tick Timer
+			//Creates Boxes Every 30 Ticks
 				if (LongTimer%30 == 0) {
 					XObject.add((rng.nextDouble() * 2) - 1);
 					YObject.add((rng.nextDouble() * 1.75) - .75);
@@ -53,12 +56,13 @@ public class Flatspace {
 					else {VXObject.add(-.002);}
 					if (rng.nextDouble() > .5) {VYObject.add(.002);}
 					else {VYObject.add(-.002);}
-					TypeObject.add(1.0); }
+					if (rng.nextDouble() < .7) {TypeObject.add(1.0); }
+					else {TypeObject.add(4.0); } }
 			//API For Object Arrays
 				int objectCount = 0;
 				while (objectCount < XObject.size()) {
 				//Bounce
-					if (TypeObject.get(objectCount) == 1.0) {
+					if (TypeObject.get(objectCount) == 1.0 || TypeObject.get(objectCount) == 4.0) {
 						if (XObject.get(objectCount) > 1.0 || XObject.get(objectCount) < -1.0) {
 							VXObject.set(objectCount, VXObject.get(objectCount) * -1.0);
 							VYObject.set(objectCount, VYObject.get(objectCount) * -1.0); }
@@ -97,9 +101,13 @@ public class Flatspace {
 				//For Explosion Entity
 					else if (TypeObject.get(objectCount) == 3.0) {
 						VXObject.set(objectCount, VXObject.get(objectCount) + 0.00001);
-						StdDraw.picture(XObject.get(objectCount), YObject.get(objectCount), "Explosion.jpg", .08, .08);
 						StdDraw.setPenColor(StdDraw.CYAN);
-						StdDraw.text(XObject.get(objectCount), YObject.get(objectCount) + .08, "+5");
+						if (VYObject.get(objectCount) != 0) {
+							StdDraw.picture(XObject.get(objectCount), YObject.get(objectCount), "Explosion.jpg", .16, .16);
+							StdDraw.text(XObject.get(objectCount), YObject.get(objectCount) + .08, "+95"); }
+						else {
+							StdDraw.picture(XObject.get(objectCount), YObject.get(objectCount), "Explosion.jpg", .08, .08);
+							StdDraw.text(XObject.get(objectCount), YObject.get(objectCount) + .08, "+5"); }
 						StdDraw.setPenColor(StdDraw.WHITE);
 						if (VXObject.get(objectCount) == 0.0002) {
 							if (XObject.size() != objectCount) {
@@ -109,7 +117,25 @@ public class Flatspace {
 								VYObject.remove(objectCount);
 								TypeObject.remove(objectCount);
 								LoopRunning = false; } } }
-				//Moves On To Next Object, If Nothing Deleted
+				//For Large Square
+					else if (TypeObject.get(objectCount) == 4.0) {
+						while (LoopRunning && DetectCount != XObject.size() && objectCount != XObject.size()) {
+							if (TypeObject.get(DetectCount) == 2.0 && Math.abs(XObject.get(DetectCount)-XObject.get(objectCount)) < .08 && Math.abs(YObject.get(DetectCount)-YObject.get(objectCount)) < .08 && rng.nextDouble() > ProbabilityLargeDelete) {
+								XObject.remove(DetectCount);
+								YObject.remove(DetectCount);
+								VXObject.remove(DetectCount);
+								VYObject.remove(DetectCount);
+								TypeObject.remove(DetectCount);
+								if (XObject.size() != objectCount) {
+									TypeObject.set(objectCount, 3.0);
+									VXObject.set(objectCount, 0.00001);
+									VYObject.set(objectCount, 0.0000001);
+									GameScore = GameScore + 95; 
+									LoopRunning = false; } }
+							else {DetectCount++;} }
+						if (LoopRunning && XObject.size() != objectCount) {
+							StdDraw.setPenColor(StdDraw.WHITE);
+							StdDraw.square(XObject.get(objectCount), YObject.get(objectCount), .08); } }
 					DetectCount = 0;
 					if (LoopRunning) {objectCount++;}
 					LoopRunning = true;}
@@ -120,11 +146,15 @@ public class Flatspace {
 						if (TypeObject.get(objectCount) == 1.0 && Math.abs(XObject.get(objectCount)-XShip) < .07 && Math.abs(YObject.get(objectCount)-YShip) < .07) {
 							GameRunning = false;
 							LoopRunning = false;}
+						if (TypeObject.get(objectCount) == 4.0 && Math.abs(XObject.get(objectCount)-XShip) < .1 && Math.abs(YObject.get(objectCount)-YShip) < .1) {
+							GameRunning = false;
+							LoopRunning = false;}
 						else {objectCount++;} }
 					LoopRunning = true;
 					objectCount = 0;}
 			//Invincible Text & Bullets Off Text
 				StdDraw.setPenColor(StdDraw.WHITE);
+				StdDraw.textRight(1, 1, "Lewpen Flatspace A1.3");
 				if (!BulletsOn && Invincible) StdDraw.textRight(1, .9, "Invincible, Bullets Off");
 				else if (Invincible) StdDraw.textRight(1, .9, "Invincible");
 			//Gray HUD With Scores
@@ -132,6 +162,8 @@ public class Flatspace {
 				StdDraw.filledRectangle(0, -1, 1.5, .2);
 				StdDraw.setPenColor(StdDraw.BLACK);
 				StdDraw.textLeft(-1, -.9, "Score: " + GameScore);
+				StdDraw.textLeft(-1, -1, "High Score: ");
+				StdDraw.text(0, -.9, ((double) Math.round(((System.currentTimeMillis() - StartTime) / 1000) * 10) / 10) + " Seconds");
 			//Debug Menu Hotkey = F3
 				if (StdDraw.isKeyPressed(114) && KeyTimeout > 30) {
 					if (DebugMenu) {DebugMenu = false; KeyTimeout = 0;}
@@ -156,14 +188,21 @@ public class Flatspace {
 					StdDraw.setPenColor(0, 0, 0); 
 					//StdDraw.filledSquare(0, 0, 2);
 					StdDraw.picture(0, 0, "Grid.jpg", 2.5, 2.5);}
+				if (XObject.size() > 100) {
+					XObject.remove(0);
+					YObject.remove(0);
+					VXObject.remove(0);
+					VYObject.remove(0);
+					TypeObject.remove(0); }
 			//Moves Timers Forward
 				LongTimer++;
+				if (LongTimer == 10000) {LongTimer = 0;}
 				if (KeyTimeout < 32) {KeyTimeout++;}
 				Thread.sleep(16); }
 		StdDraw.setPenColor(StdDraw.RED);
 		StdDraw.filledRectangle(0, -1, 1.5, .2);
 		StdDraw.setPenColor(StdDraw.BLACK);
-		StdDraw.text(0, -.9, "You died! ESC to restart.");
+		StdDraw.text(0, -.9, "You survived for " + ((double) Math.round(((System.currentTimeMillis() - StartTime) / 1000) * 10) / 10) + " seconds! Press ESC to restart.");
 		StdDraw.text(0, -1, "Score: " + GameScore);
 		StdDraw.show(0);
 		while (!StdDraw.isKeyPressed(27)) {}
